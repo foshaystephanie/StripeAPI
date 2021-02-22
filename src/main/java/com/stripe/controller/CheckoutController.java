a5f03cf80986e9f8bbd514188fa81d9e0f975f8d
Stephanie Foshay <foshaystephanie@gmail.com>
2021-02-21 19:06:34 -0600

    working stripe api

--- src/main/java/com/stripe/controller/CheckoutController.java
+++ src/main/java/com/stripe/controller/CheckoutController.java
@@ -1,5 +1,25 @@
 package com.stripe.controller;
 
-public class CheckoutController {
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.stripe.models.ChargeRequest;
+
+@Controller
+public class CheckoutController {
+	
+	private String stripePublicKey = System.getenv("STRIPE_PUBLIC_KEY");
+	
+	
+
+    @RequestMapping("/checkout")
+    public String checkout(Model model) {
+        model.addAttribute("amount", 50 * 100); // in cents
+        model.addAttribute("stripePublicKey", stripePublicKey);
+        model.addAttribute("currency", ChargeRequest.Currency.USD);
+        return "checkout";
+    }
 }
---
+++ src/main/java/com/stripe/service/StripeService.java
@@ -0,0 +1,40 @@
+package com.stripe.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import com.stripe.Stripe;
+import com.stripe.exception.AuthenticationException;
+import com.stripe.exception.CardException;
+import com.stripe.exception.InvalidRequestException;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import com.stripe.models.ChargeRequest;
+
+@Service
+public class StripeService{
+	private String secretKey = System.getenv("STRIPE_SECRET_KEY");
+    
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
+    public Charge charge(ChargeRequest chargeRequest) 
+      throws AuthenticationException, InvalidRequestException,
+         CardException, StripeException {
+        Map<String, Object> chargeParams = new HashMap<>();
+        chargeParams.put("amount", chargeRequest.getAmount());
+        chargeParams.put("currency", chargeRequest.getCurrency());
+        chargeParams.put("description", chargeRequest.getDescription());
+        chargeParams.put("source", chargeRequest.getStripeToken());
+        return Charge.create(chargeParams);
+    }
+}
